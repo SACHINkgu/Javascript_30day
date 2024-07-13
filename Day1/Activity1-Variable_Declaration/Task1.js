a3fb54d758f31fe55657494a93598621f0fa7295
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-13 17:04:43 +0000

    Task 1,2

--- Day1/Activity1-Variable_Declaration/Task1.js
+++ Day1/Activity1-Variable_Declaration/Task1.js
@@ -1,3 +1,6 @@
+// Activity 1: Variable Declaration
+// Task 1: Declare a variable using vor , assign it a number, and log the value to the console.
+
 var number
 number = 7;
 console.log(number);
--- Day1/Activity1-Variable_Declaration/Task2.js
+++ Day1/Activity1-Variable_Declaration/Task2.js
@@ -1,3 +1,6 @@
+// Activity 1: Variable Declaration
+// Task 2: Declare a variable using let , assign it a string, and log the value to the console.
+
 let naam;
 naam = "Sachin"
 console.log(naam)
