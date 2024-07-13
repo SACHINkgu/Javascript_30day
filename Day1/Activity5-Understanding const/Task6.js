e1e3329de5d95a872dcaa63b898288611cf24eb7
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-13 16:56:07 +0000

    Undestanding constant

---
+++ Day1/Activity5-Understanding const/Task6.js
@@ -0,0 +1,10 @@
+// Activity 5: Understanding const
+// Task 6: Try reassigning a variable declared with const and observe the error.
+
+const naam = "Javascript-30";
+console.log("Name of Challenge is :", naam);
+
+naam ="Java";
+console.log("Name of Challenge is :",naam);
+
+//naam ="Java"; TypeError: Assignment to constant variable.
