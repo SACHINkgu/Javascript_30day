86e85c6581bbec4c25c5ded7a89306116082eaa0
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-13 17:07:33 +0000

    Task3- Constant Declaration

--- Day1/Activity2-Constant_Declaration/Task3.js
+++ Day1/Activity2-Constant_Declaration/Task3.js
@@ -1,2 +1,5 @@
-const ans = false;
+// Activity 2: Constant Declaration
+// Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console
+
+const ans = true;
 console.log(ans);
