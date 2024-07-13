bb779feeed4259e9119ae3f5ce69c867e1760bb7
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-13 16:44:41 +0000

    Activity 4: Reassigning Variables

---
+++ Day1/Activity4-Reassigning Variables/Task5.js
@@ -0,0 +1,8 @@
+// Activity 4: Reassigning Variables
+// Task 5: Declare a variable using let , assign it an initial value, reassign a new value, and log both values to the console.
+
+let age = 22;
+console.log("Previous age:",age);
+
+age = 23;
+console.log("Present age:",age);
