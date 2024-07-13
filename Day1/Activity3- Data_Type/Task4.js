4cd7c79bda4f67d1060ba817fcbc861fe18616aa
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-13 16:34:47 +0000

    Data-types

---
+++ Day1/Activity3- Data_Type/Task4.js
@@ -0,0 +1,22 @@
+// Activity 3: Data Types
+// Task 4: Create variables of different data types (number, string, boolean, object, array) and log each variable's type using the typcof operator.
+
+let number = 7;
+let work = "Techcompany";
+const isbool = true ;
+let data ={ naam :"Sachin", age:7};
+let array = [2,3,4,5,6];
+
+console.log(typeof number);
+console.log(typeof work);
+console.log(typeof isbool);
+console.log(typeof data);
+console.log(typeof array);
+
+// Outputs
+
+// number
+// string
+// boolean
+// object
+// object
