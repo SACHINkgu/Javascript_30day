b169061d6f966f042aa08592856f52a535b70af4
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-14 07:45:12 +0000

    finding remainder

---
+++ Day2: Operators/Activity 1- Arithmetic Operation/Task5.js
@@ -0,0 +1,12 @@
+// Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console.
+
+// Operator	    | Description	                                                                | Example
+// Remainder(%)	| Binary operator. Returns the integer remainder of dividing the two operands.	| 12 % 5 returns 2. 
+
+let a = 12;
+let b = 5;
+
+remainder = a%b;
+console.log("Remainder is : ",remainder);
+
+// Remainder is :  2
