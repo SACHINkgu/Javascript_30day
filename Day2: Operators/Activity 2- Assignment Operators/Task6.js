aa42fd089c8ad68b496f35fb31ab272f9ee13401
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-14 08:32:21 +0000

    +=

---
+++ Day2: Operators/Activity 2- Assignment Operators/Task6.js
@@ -0,0 +1,10 @@
+// Task 6: Use the += operator to add a number to a variable and log the result to the console.
+
+// The addition assignment (+=) operator performs addition (which is either numeric addition or string concatenation) 
+// on the two operands and assigns the result to the left operand.
+
+let a = 7;
+let b ="hello";
+
+console.log((a += 2));
+console.log((b +=' world')); // Concatenation
