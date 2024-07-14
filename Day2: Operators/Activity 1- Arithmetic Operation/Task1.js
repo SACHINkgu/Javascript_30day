3b0c5f9e5ddbbfd03fe6c838bad8981938496860
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-14 07:29:01 +0000

    Task 1,2

---
+++ Day2: Operators/Activity 1- Arithmetic Operation/Task1.js
@@ -0,0 +1,8 @@
+// Activity 1: Arithmetic Operation
+// Tack 1: Write a program to add two nunbers and log the result to the console.
+
+let a =7;
+let b =4;
+
+let sum = a+b;
+console.log("Sum of a and b is :",sum);
---
+++ Day2: Operators/Activity 1- Arithmetic Operation/Task2.js
@@ -0,0 +1,10 @@
+// Activity 1: Arithmetic Operation
+// Tack 2: Write a program to subtract two number and log the result to the console.
+
+let  a = 7;
+let b = 4;
+
+let ans = a-b;
+let out = b-a;
+
+console.log(ans,out); //3 -3
