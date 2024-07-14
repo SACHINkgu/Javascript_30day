1851610e43e44220897a30a223f0f3f7316ed1c2
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-14 11:00:55 +0000

    >= and <=

---
+++ Day2: Operators/Activity 3- Comparison Operators/Task9.js
@@ -0,0 +1,10 @@
+// Task 9: Write a program to compare two numbers using >= and <= and log the result to the console.
+
+let a= 8;
+let b = 2;
+let c =8;
+let d = 9;
+console.log(a >= b);
+console.log(a <= d);
+console.log(a<=c);
+console.log(a<=b);
