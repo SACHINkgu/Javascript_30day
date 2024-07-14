6464ed9ce33ca418cfac5f33b704f977e7c874be
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-14 07:48:36 +0000

    The modulus operator (%) returns the division remainder.

--- Day2: Operators/Activity 1- Arithmetic Operation/Task5.js
+++ Day2: Operators/Activity 1- Arithmetic Operation/Task5.js
@@ -1,7 +1,9 @@
 // Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console.
 
-// Operator	    | Description	                                                                | Example
-// Remainder(%)	| Binary operator. Returns the integer remainder of dividing the two operands.	| 12 % 5 returns 2. 
+// Operator	    | Description	                                                | Example
+// Remainder(%)	| Returns the integer remainder of dividing the two operands.	| 12 % 5 returns 2. 
+
+// Note : The modulus operator (%) returns the division remainder.
 
 let a = 12;
 let b = 5;
