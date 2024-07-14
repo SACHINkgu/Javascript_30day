93aed4b5b6116b6d9e0034644bf4dec4d45f6070
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-14 11:07:07 +0000

    == and ===

---
+++ Day2: Operators/Activity 3- Comparison Operators/Task10.js
@@ -0,0 +1,11 @@
+// Task 10: Write a program to compare two numbers using == and === and log the result to the console.
+
+// === — strict equality (triple equals)
+// ==  — loose equality (double equals)
+
+const a = 9 ;
+const b = "9" ;
+console.log(a == b); // true
+console.log(a === b); // false
+
+console.log(a === a); // true
