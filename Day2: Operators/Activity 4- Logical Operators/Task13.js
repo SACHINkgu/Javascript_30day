2dc1f7bd885e5df0ea465329996c31d44a8a6b1e
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-14 11:20:02 +0000

    Logical NOT (!)

---
+++ Day2: Operators/Activity 4- Logical Operators/Task13.js
@@ -0,0 +1,6 @@
+// Task 13: Write a program that uses the ! operator to negate a condition and log the result to the console.
+
+const a = 3;
+const b = 2;
+
+console.log(!(a > 0 || b > 0)); // false
