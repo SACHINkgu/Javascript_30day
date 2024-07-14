f49b1e8f810db5be070da4227f3945c2445b7361
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-14 09:07:31 +0000

    Comparison (>,<) operators

---
+++ Day2: Operators/Activity 3- Comparison Operators/Task8.js
@@ -0,0 +1,15 @@
+// Activity 3: Comparison Operators
+// Task 8: Write a program to compare two numbers using > and < and log the result to the console.
+
+let a = 2;
+let b = 3;
+// console.log(a>b);
+
+if(a>b){
+    console.log(`${a} is greater than ${b}`); // better way to console your result 
+    
+}
+else{
+    console.log(`${a} is smaller than ${b}`);
+    //console.log("a is smaller than b is : ",b);
+}
