16935677003737387c03c28fc5d15f7dc42c1ee7
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-14 11:29:29 +0000

    Ternary operator

---
+++ Day2: Operators/Activity 5- Ternary Operator/Task14.js
@@ -0,0 +1,7 @@
+// Task 14: Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console.
+
+const a = 2;
+
+let result =  a > 0 ? "Number is positive" : "Number is negative";
+console.log( result );
+// Number is positive
