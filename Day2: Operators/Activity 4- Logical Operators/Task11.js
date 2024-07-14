030664a3fbd5fd0c1d6e83b4fbe6111487c3a746
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-14 11:14:04 +0000

    && operator

---
+++ Day2: Operators/Activity 4- Logical Operators/Task11.js
@@ -0,0 +1,12 @@
+// Task 11: Write a program that uses the && operator to combine two conditions and log the result to the console.
+
+const a = 9;
+const b = 4;
+const c = 10;
+
+if((a>b) && (a<c)){
+    console.log("Yes");
+}
+else{
+    console.log("No");
+}
