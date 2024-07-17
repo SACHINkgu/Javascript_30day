2c56e5860a4a98219ea3f17cfbca6a044f088c0e
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-17 09:29:16 +0000

    task4

---
+++ Day3: Control Structures/Activity 3- Switch Case/Task4.js
@@ -0,0 +1,30 @@
+let dayNumber = 3; // You can change this number to test with different values
+let dayName;
+
+switch(dayNumber) {
+    case 1:
+        dayName = "Sunday";
+        break;
+    case 2:
+        dayName = "Monday";
+        break;
+    case 3:
+        dayName = "Tuesday";
+        break;
+    case 4:
+        dayName = "Wednesday";
+        break;
+    case 5:
+        dayName = "Thursday";
+        break;
+    case 6:
+        dayName = "Friday";
+        break;
+    case 7:
+        dayName = "Saturday";
+        break;
+    default:
+        dayName = "Invalid day number. Please enter a number between 1 and 7.";
+}
+
+console.log(dayName);
