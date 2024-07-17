975fe1ed06cbcd2a94bc4a9ebb7ca8dd8c450534
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-17 11:31:39 +0000

    task4

---
+++ Day5: Functions/Activity 2- Function Expression/Task4.js
@@ -0,0 +1,8 @@
+// Task 4: Write a function expression to concatenate two strings and return the result.
+const concatenateStrings = function(str1, str2) {
+    return str1 + str2; // Return the concatenated result
+};
+
+// Example usage:
+const result = concatenateStrings("Hello, ", "world!"); // Call the function with two strings
+console.log(result); // Output: "Hello, world!"
