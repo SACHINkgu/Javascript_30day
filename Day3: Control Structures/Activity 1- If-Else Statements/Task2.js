62b88744945f7389c904c4c0b4cec054683d0628
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-17 08:33:55 +0000

    Task2

---
+++ Day3: Control Structures/Activity 1- If-Else Statements/Task2.js
@@ -0,0 +1,9 @@
+// • Task 2: Write a program to check if a person is eligible to vote (age >= 18) and log the result to the console.
+
+let age = 12 ;
+if(age >= 18){
+    console.log(`Eligible to Vote`);
+}
+else{
+    console.log(`Not-eligible to Vote`);
+}
