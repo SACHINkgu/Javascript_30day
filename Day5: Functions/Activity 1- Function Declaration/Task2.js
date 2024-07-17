2a6af5300b1487964d924584fefc6b65efa1406e
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-17 11:22:05 +0000

    task2

---
+++ Day5: Functions/Activity 1- Function Declaration/Task2.js
@@ -0,0 +1,9 @@
+// . Task 2: Write a function to calculate the square of a number and return the result.
+
+function square(number) {
+    return number * number; // Calculate and return the square
+}
+
+// Example usage:
+let result = square(5); // Call the function with a number
+console.log(result); // Output: 25
