a1946e288eac1072f23bd7703268dae42894810b
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-17 09:44:26 +0000

    task7

--- Day3: Control Structures/Activity 5- Combining Conditions/Task7.js
+++ Day3: Control Structures/Activity 5- Combining Conditions/Task7.js
@@ -1,7 +1,7 @@
 // . Task 7: Write a program to check if a year is a leap year using multiple conditions (divisible by 4, but not 100 unless also divisible by 400) and log
 // the result to the console.
 
-let year = 2023; // You can change this year to test with different values
+let year = 2024; // You can change this year to test with different values
 
 let isLeapYear = (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) ? "Leap Year" : "Not a Leap Year";
 
