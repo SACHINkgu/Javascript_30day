82487f4205785a9d3ec562d27e8bc9f769c80f37
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-17 09:14:52 +0000

    Task3

---
+++ Day3: Control Structures/Activity 2- Nested If-Else Statements/Task3.js
@@ -0,0 +1,12 @@
+// • Task 3: Write a program to find the largest of three numbers using nested if-else statements
+let num1 = 1;
+let num2 = 4;
+let num3 = 9;
+
+if (num1 >= num2 && num1 >= num3) {
+    console.log( num1);
+} else if (num2 >= num1 && num2 >= num3) {
+    console.log( num2);
+} else {
+    console.log( num3);
+}
