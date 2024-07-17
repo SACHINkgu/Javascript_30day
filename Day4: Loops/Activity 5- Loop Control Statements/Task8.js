c22cc61370a64b3e9b56aef7ba2d1624d6636372
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-17 10:39:05 +0000

    task8

---
+++ Day4: Loops/Activity 5- Loop Control Statements/Task8.js
@@ -0,0 +1,7 @@
+// . Task 8: Write a program to print numbers from 1 to 10, but skip the number 5 using the continue statement.
+for (let i = 1; i <= 10; i++) {
+    if (i === 5) {
+        continue; // Skip the number 5
+    }
+    console.log(i); // Print the number
+}
