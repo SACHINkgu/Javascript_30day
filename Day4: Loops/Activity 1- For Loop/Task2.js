ee7703c294f7140a57de815524def3251cea7530
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-17 09:54:29 +0000

    task2

---
+++ Day4: Loops/Activity 1- For Loop/Task2.js
@@ -0,0 +1,6 @@
+// . Task 2: Write a program to print the multiplication table of 5 using a for loop.
+let number = 5; // The number for which the multiplication table will be printed
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${number} x ${i} = ${number * i}`);
+}
