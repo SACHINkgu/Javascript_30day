031b206d8331182cfa2e347d062c0c3c056a8e58
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-17 09:40:01 +0000

    task6

---
+++ Day3: Control Structures/Activity 4- Conditional (Ternary) Operator/Task6.js
@@ -0,0 +1,6 @@
+// . Task 6: Write a program that uses the ternary operator to check if a number is even or odd and log the result to the console.
+let number = 42; // You can change this number to test with different values
+
+let result = (number % 2 === 0) ? "Even" : "Odd";
+
+console.log(result); // Output: "Even"
