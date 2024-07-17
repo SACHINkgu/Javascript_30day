ecf1a40c7cad8ee209498abd684e15053417753b
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-17 11:18:20 +0000

    task1

---
+++ Day5: Functions/Activity 1- Function Declaration/Task1.js
@@ -0,0 +1,25 @@
+// . Task 1: Write a function to check if a number is even or odd and log the result to the console.
+
+function check(num){
+    if(num % 2 == 0){
+        console.log(`Even`);
+    }
+    else{
+        console.log(`ODD`);
+    }
+}
+
+let ans = check(4);
+
+
+// function checkEvenOrOdd(number) {
+//     if (number % 2 === 0) {
+//         console.log(`${number} is Even`);
+//     } else {
+//         console.log(`${number} is Odd`);
+//     }
+// }
+
+// // Example usage:
+// checkEvenOrOdd(4); // Output: "4 is Even"
+// checkEvenOrOdd(7); // Output: "7 is Odd"
