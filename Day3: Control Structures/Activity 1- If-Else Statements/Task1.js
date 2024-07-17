23a18aab577569a6208643b5d2feb5c31f73a318
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-17 08:26:47 +0000

    Task1

---
+++ Day3: Control Structures/Activity 1- If-Else Statements/Task1.js
@@ -0,0 +1,12 @@
+// Task 1: Write a program to check if a number is positive, negative, or zero, and log the result to the console.
+
+let num = 4;
+if(num>0){
+    console.log(`Number ${num} is positive`);
+}
+else if(num === 0){
+    console.log(`Number ${num} is equal to zero`);
+}
+else{
+    console.log(`Number ${num} is negative`);
+}
