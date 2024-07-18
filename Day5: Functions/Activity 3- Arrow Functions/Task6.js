38d1e527c0f891b8b79aa2c44fa6ca87f264e192
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-18 19:25:20 +0000

    arrow  fun

---
+++ Day5: Functions/Activity 3- Arrow Functions/Task6.js
@@ -0,0 +1,7 @@
+
+const checkchar = (str, char) => str.includes(char) 
+
+const myString = "Hello, world!";
+const characterToCheck = "o";
+const result = checkchar(myString, characterToCheck);
+console.log(result);
