4c9ed45cf3f51b742491aa32ce5c14af7d4a4942
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-07-18 18:53:33 +0000

    task5

---
+++ Day5: Functions/Activity 3- Arrow Functions/Task5.js
@@ -0,0 +1,9 @@
+// Arrow function to calculate the sum of two numbers
+const sum = (a, b) => a + b;
+
+// Example usage
+const num1 = 3;
+const num2 = 5;
+const result = sum(num1, num2);
+
+console.log(`The sum of ${num1} and ${num2} is ${result}`); // Output: The sum of 3 and 5 is 8
